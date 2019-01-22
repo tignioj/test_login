b6f0799f417580f0ff3e8af4c4fc074ec722d4b3
mike <8598979947@qq.com>
2019-01-22 11:04:31 +0800

    fix the problem that some borwser could not jump to the next video

--- zhs/ChromeVersionPassVideo.js
+++ zhs/ChromeVersionPassVideo.js
@@ -11,32 +11,30 @@ function speedUp_muted_bq() {
     $(".line1bq")[0].click();
 }
 
-
 function autoChooseTest() {
     //check if the test window is open
-    if (!document.getElementById("tmDialog_iframe")) {
+    var video = document.getElementById("vjs_mediaplayer_html5_api");
+    if (!video.paused) {
+        console.log(video.paused);
         return;
     } else {
-        //choose right answer
-        var myradio = document.getElementById("tmDialog_iframe").contentWindow.document.getElementsByClassName("answerOption");
-        for (var i = 0; i < myradio.length; i++) {
-            if (myradio[i].getElementsByTagName("input")[0].getAttribute("_correctanswer") == "1")
-                myradio[i].getElementsByTagName("input")[0].click();
-            //double  click checked;
-            if (myradio[i].getElementsByTagName("input")[0].getAttribute("checked") == "checked")
-                myradio[i].getElementsByTagName("input")[0].click();
+        try {
+            //choose right answer
+            var myradio = document.getElementById("tmDialog_iframe").contentWindow.document.getElementsByClassName("answerOption");
+            for (var i = 0; i < myradio.length; i++) {
+                if (myradio[i].getElementsByTagName("input")[0].getAttribute("_correctanswer") == "1") {
+                    $(".popbtn_cancel")[0].click();
+                    myradio[i].getElementsByTagName("input")[0].click();
+                }
+                //double  click checked;
+                if (myradio[i].getElementsByTagName("input")[0].getAttribute("checked") == "checked") {
+                    myradio[i].getElementsByTagName("input")[0].click();
+                }
+            }
+        } catch (err) {
+            console.log(err);
         }
     }
-
-
-    //close the window of answer
-    for (var i = 0; i < $(".popbtn_cancel span").length; i++) {
-        if ($(".popbtn_cancel span")[i].innerHTML == "关闭" || $(".popbtn_cancel span")[i].innerHTML == "Close") {
-            $(".popbtn_cancel span")[i].click();
-            //console.log($(".popbtn_cancel span")[i].innerHTML);
-        }
-    }
-
 }
 
 function nextVideo() {
@@ -47,17 +45,31 @@ function nextVideo() {
             speedUp_muted_bq();
         }, 1500);
     }
-    if ((video.currentTime / video.duration) >= 1) {
+    var p = document.getElementsByClassName("progressbar")[0];
+    var width = document.defaultView.getComputedStyle(p, null).width;
+
+    var str = p.getAttribute("style");
+    var regex = new RegExp("[0-9]+");
+    var obj = regex.exec(str);
+    var isProcessFinish = false;
+    if (obj != undefined) {
+        console.log(obj);
+        if (parseInt(obj[0]) >= 100) {
+            isProcessFinish = true;
+        } else {
+            isProcessFinish = false;
+        }
+    }
+
+    if ((video.currentTime / video.duration) >= 1 || isProcessFinish) {
         console.log("Finish");
         $(".tm_next_lesson")[0].click();
         setTimeout(function() {
             speedUp_muted_bq();
         }, 1500);
     }
-    //If a chapter is finish, test if next video  is as same to the current video
 }
 
-//Start 
 speedUp_muted_bq();
 var myInterval = setInterval(function() {
     autoChooseTest();
